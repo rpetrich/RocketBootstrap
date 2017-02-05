fea77d6211e42c9095eb545c4f589060f59674b1
Ryan Petrich <rpetrich@gmail.com>
2017-02-05 02:46:53 -0500

    Adjust header documentation

--- rocketbootstrap.h
+++ rocketbootstrap.h
@@ -8,18 +8,33 @@
 __BEGIN_DECLS
 #ifndef ROCKETBOOTSTRAP_LOAD_DYNAMIC
 
-kern_return_t rocketbootstrap_look_up(mach_port_t bp, const name_t service_name, mach_port_t *sp);
+// Look up a port by service name
+kern_return_t rocketbootstrap_look_up(mach_port_t bootstrap_port, const name_t service_name, mach_port_t *out_service_port);
+
+// Grant system-wide access to a particular service name
+// Note: Will return an error if called from within a sandboxed process
+kern_return_t rocketbootstrap_unlock(const name_t service_name);
+// Register a port with and grant system-wide access to a particular service name
+// Note: Will return an error if called from within a sandboxed process
+kern_return_t rocketbootstrap_register(mach_port_t bootstrap_port, name_t service_name, mach_port_t service_port);
 
 
-kern_return_t rocketbootstrap_unlock(const name_t service_name); // Errors if not in a privileged process such as SpringBoard or backboardd
-kern_return_t rocketbootstrap_register(mach_port_t bp, name_t service_name, mach_port_t sp); // Errors if not in a privileged process such as SpringBoard or backboardd
+// CFMessagePort helpers
 
 #ifdef __COREFOUNDATION_CFMESSAGEPORT__
+// Acquire access to a system-wide CFMessagePort service
 CFMessagePortRef rocketbootstrap_cfmessageportcreateremote(CFAllocatorRef allocator, CFStringRef name);
+// Expose access to a CFMessagePort service
+// Note: Will return an error if called from within a sandboxed process
 kern_return_t rocketbootstrap_cfmessageportexposelocal(CFMessagePortRef messagePort);
 #endif
 
+
+// CPDistributedMessagingCenter helpers
+
 #ifdef __OBJC__
+// Unlock access to a system-wide CPDistributedMessagingCenter service
+// Note: Server processes may only run inside privileged processes
 @class CPDistributedMessagingCenter;
 void rocketbootstrap_distributedmessagingcenter_apply(CPDistributedMessagingCenter *messaging_center);
 #endif
