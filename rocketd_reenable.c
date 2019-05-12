12d4f5c89e09d65dd202df044fbece5c4f3083f6
Ryan Petrich <rpetrich@gmail.com>
2019-05-12 16:35:48 -0400

    Support pointer authentication when calling jb_connect/jb_fix_suid_now/jb_disconnect (which may be required, not sure?)

--- rocketd_reenable.c
+++ rocketd_reenable.c
@@ -2,15 +2,29 @@
 #import <dlfcn.h>
 #import <stdio.h>
 
+#ifdef __LP64__
+#if __has_include(<ptrauth.h>)
+#include <ptrauth.h>
+#define sign_function(ptr) ({ \
+	__typeof__(ptr) _ptr = ptr; \
+	ptr ? ptrauth_sign_unauthenticated(_ptr, ptrauth_key_function_pointer, 0) : _ptr; \
+})
+#else
+#define sign_function(ptr) ptr
+#endif
+#else
+#define sign_function(ptr) ptr
+#endif
+
 #ifdef __arm64__
 #import "libjailbreak_xpc.h"
 static int fix_setuid(void)
 {
 	void *libjailbreak = dlopen("/usr/lib/libjailbreak.dylib", RTLD_LAZY);
 	if (libjailbreak) {
-		jb_connection_t (*jb_connect)(void) = dlsym(libjailbreak, "jb_connect");
-		int (*jb_fix_setuid_now)(jb_connection_t connection, pid_t pid) = dlsym(libjailbreak, "jb_fix_setuid_now");
-		void (*jb_disconnect)(jb_connection_t connection) = dlsym(libjailbreak, "jb_disconnect");
+		jb_connection_t (*jb_connect)(void) = sign_function(dlsym(libjailbreak, "jb_connect"));
+		int (*jb_fix_setuid_now)(jb_connection_t connection, pid_t pid) = sign_function(dlsym(libjailbreak, "jb_fix_setuid_now"));
+		void (*jb_disconnect)(jb_connection_t connection) = sign_function(dlsym(libjailbreak, "jb_disconnect"));
 		if (jb_connect && jb_fix_setuid_now && jb_disconnect) {
 			jb_connection_t connection = jb_connect();
 			if (connection) {
