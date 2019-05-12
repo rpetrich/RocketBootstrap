5d030a1517c532a8304af4a6d8f6eed542b3b6ce
Ryan Petrich <rpetrich@gmail.com>
2019-05-12 16:28:35 -0400

    Support pointer authentication in clients when ROCKETBOOTSTRAP_LOAD_DYNAMIC is set in client libraries (requires recompile to take advantage on arm64e)

--- rocketbootstrap_dynamic.h
+++ rocketbootstrap_dynamic.h
@@ -2,16 +2,43 @@
 // Set ROCKETBOOTSTRAP_LOAD_DYNAMIC and then include rocketbootstrap.h
 #include <dlfcn.h>
 
+#if __has_include(<ptrauth.h>)
+#include <ptrauth.h>
+
+__attribute__((unused))
+static void *_rocketbootstrap_dlsym_func(const char *name) {
+	void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
+	if (handle) {
+		void *result = dlsym(handle, name);
+		if (result) {
+			return ptrauth_sign_unauthenticated(ptrauth_strip(result, ptrauth_key_function_pointer), ptrauth_key_function_pointer, 0);
+		}
+	}
+	return NULL;
+}
+
+#else
+
+__attribute__((unused))
+static void *_rocketbootstrap_dlsym_func(const char *name) {
+	void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
+	if (handle) {
+		return dlsym(handle, name);
+	}
+	return NULL;
+}
+
+#endif
+
 __attribute__((unused))
 static kern_return_t rocketbootstrap_look_up(mach_port_t bp, const name_t service_name, mach_port_t *sp)
 {
 	static kern_return_t (*impl)(mach_port_t bp, const name_t service_name, mach_port_t *sp);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_look_up");
-		if (!impl)
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_look_up");
+		if (!impl) {
 			impl = bootstrap_look_up;
+		}
 	}
 	return impl(bp, service_name, sp);
 }
@@ -21,11 +48,10 @@ static kern_return_t rocketbootstrap_unlock(const name_t service_name)
 {
 	static kern_return_t (*impl)(const name_t service_name);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_unlock");
-		if (!impl)
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_unlock");
+		if (!impl) {
 			return -1;
+		}
 	}
 	return impl(service_name);
 }
@@ -36,11 +62,10 @@ static kern_return_t rocketbootstrap_register(mach_port_t bp, name_t service_nam
 {
 	static kern_return_t (*impl)(mach_port_t bp, name_t service_name, mach_port_t sp);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_register");
-		if (!impl)
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_register");
+		if (!impl) {
 			impl = bootstrap_register;
+		}
 	}
 	return impl(bp, service_name, sp);
 }
@@ -52,11 +77,10 @@ static CFMessagePortRef rocketbootstrap_cfmessageportcreateremote(CFAllocatorRef
 {
 	static CFMessagePortRef (*impl)(CFAllocatorRef allocator, CFStringRef name);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_cfmessageportcreateremote");
-		if (!impl)
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_cfmessageportcreateremote");
+		if (!impl) {
 			impl = CFMessagePortCreateRemote;
+		}
 	}
 	return impl(allocator, name);
 }
@@ -65,11 +89,10 @@ static kern_return_t rocketbootstrap_cfmessageportexposelocal(CFMessagePortRef m
 {
 	static kern_return_t (*impl)(CFMessagePortRef messagePort);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_cfmessageportexposelocal");
-		if (!impl)
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_cfmessageportexposelocal");
+		if (!impl) {
 			return -1;
+		}
 	}
 	return impl(messagePort);
 }
@@ -82,9 +105,7 @@ static void rocketbootstrap_distributedmessagingcenter_apply(CPDistributedMessag
 {
 	static void (*impl)(CPDistributedMessagingCenter *messagingCenter);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_distributedmessagingcenter_apply");
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_distributedmessagingcenter_apply");
 		if (!impl)
 			return;
 	}
@@ -98,9 +119,7 @@ static xpc_connection_t rocketbootstrap_xpc_connection_create(const char *name, 
 {
 	static xpc_connection_t (*impl)(const char *name, dispatch_queue_t targetq, uint64_t flags);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_xpc_connection_create");
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_xpc_connection_create");
 		if (!impl)
 			return NULL;
 	}
@@ -112,9 +131,7 @@ static xpc_object_t rocketbootstrap_xpc_connection_copy_application_identifier(x
 {
 	static xpc_object_t (*impl)(xpc_connection_t connection);
 	if (!impl) {
-		void *handle = dlopen("/usr/lib/librocketbootstrap.dylib", RTLD_LAZY);
-		if (handle)
-			impl = dlsym(handle, "rocketbootstrap_xpc_connection_copy_application_identifier");
+		impl = _rocketbootstrap_dlsym_func("rocketbootstrap_xpc_connection_copy_application_identifier");
 		if (!impl)
 			return NULL;
 	}
